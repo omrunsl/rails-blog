9104fe139e6bed17f02328bb2c4dd7eaad9031f1
Rails Blog <omerunsal88@gmail.com>
2018-06-02 04:34:11 +0300

    Articles Controllerda düzenleme yapıldı

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -18,15 +20,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(article_params)
       flash[:notice] = "Article güncellendi"
       redirect_to @article
@@ -36,13 +38,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = "Article başarıyla silindi"
     redirect_to @article
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
       params.require(:article).permit(:title, :description)
     end
