72230252dcfcf275020efebbd92094bd79f8ac7f
Rails Blog <omerunsal88@gmail.com>
2018-06-06 01:23:25 +0300

    Admin ayarları yapıldı

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
     end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = "Bu işlem için yetkiniz yok"
       redirect_to root_path
     end
